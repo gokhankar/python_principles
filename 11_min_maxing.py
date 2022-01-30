ba6f08920a8d05bb9e9f6c32891a76fe23ab6276
Your Name <gokhankaracay83@hotmail.com>
2022-01-30 23:07:17 +0300

    turkish 11, 12,13

---
+++ 11_min_maxing.py
@@ -0,0 +1,21 @@
+"""
+Min-maxing
+Define a function named largest_difference that takes a list of numbers as its only parameter.
+
+Your function should compute and return the difference between the largest and 
+smallest number in the list.
+
+For example, the call largest_difference([1, 2, 3]) should return 2 because 3 - 1 is 2.
+
+You may assume that no numbers are smaller or larger than -100 and 100.
+"""
+"""
+largest_difference adında bir fonksiyon yazın, bu fonksiyon tek parametre alsın ve 
+bu parametre sayılardan oluşan bir liste olsun.
+Fonksiyonumuz listedeki en büyük ve en küçük sayı arasındaki farkı hesaplayıp dönsün.
+
+mesela, largest_difference([1, 2, 3]) çağrıldığında, fonksiyon 2 dönmelidir, çünkü
+3-1=2 dir.
+
+Listedeki sayılarımız ise -100 den küçük olmasın ve 100 den büyük olmasın.
+"""
---
+++ 12_divisible_by_3.py
@@ -0,0 +1,18 @@
+"""
+Divisible by 3
+Define a function named div_3 that returns True if its single integer parameter 
+is divisible by 3 and False otherwise.
+
+For example, div_3(6) is True because 6/3 does not leave any remainder. 
+However div_3(5) is False because 5/3 leaves 2 as a remainder.
+"""
+
+"""
+div_3 adında bir fonksiyon yazın, tek integer parametresi olsun. Eğer bu parametre 
+3 e bölünebilirse True, yoksa False dönsün.
+
+Mesela div_3(6) True dönsün çünkü 6/3 ün kalanı sıfırdır. div_3(5) ise False çünkü 
+5/3 kalanı 2 dir.
+
+
+"""
